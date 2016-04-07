d3e8cfb739c4fd992bfafddaa0a8e7325dd3f44f
Ryder Timberlake <wrtimberlake@gmail.com>
2016-04-07 16:50:05 -0400

    Built out ships controller

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -1,3 +1,44 @@
 class ShipsController < ApplicationController
-  
+  before_action :require_user, except: [:index]
+
+  def index
+    if current_user
+
+    else
+
+    end
+  end
+
+  def show
+  end
+
+  def new
+    @ship = Ship.new
+  end
+
+  def create
+    @ship = current_user.ships.create(ship_params)
+
+    respond_to do |format|
+      if @ship.save
+        format.html { redirect_to user_ships_path, notice: 'ship was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def destroy
+    @ship.destroy
+    respond_to do |format|
+      format.html { redirect_to user_ships_path, notice: 'Ship was successfully destroyed.' }
+    end
+  end
+
+  private
+
+  def ship_params
+    params.require(:ship).permit(:body)
+  end
+
 end
