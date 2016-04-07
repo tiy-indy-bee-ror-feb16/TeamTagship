955950be9f9cd9c20f41588edd98e5571abef51e
Ryder Timberlake <wrtimberlake@gmail.com>
2016-04-07 16:15:44 -0400

    Pulled over authentication helper methods, body of SessionsController and UsersController from Reddit project

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+  before_action :disallow_user, :only => [:new, :create]
+  before_action :require_user, :only => [:destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    if @user = User.find_by_email(params[:session][:email]).try(:authenticate, params[:session][:password])
+      session[:user_id] = @user.id
+      flash[:success] = "Hello, #{@user.username}."
+      redirect_to :users
+    else
+      flash[:warning] = "Incorrect email or password."
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Logged out successfully."
+    redirect_to :users
+  end
 end
