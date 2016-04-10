276d2d246fa84f7f9a636b50235ac38fa1d9e752
Ryder Timberlake <wrtimberlake@gmail.com>
2016-04-10 16:34:53 -0400

    Changed seed logic by adding a rescue block that creates another user if the first user shares username with an existing user. Good candidate for a recursive solution, but that's all for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,20 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-50.times do
-  User.create!(
-  username: Faker::Internet.user_name,
-  email: Faker::Internet.safe_email,
-  password: Faker::Internet.password(8)
-  )
-
-  User.all
+100.times do
+  begin
+    User.create!(
+    username: Faker::Internet.user_name,
+    email: Faker::Internet.safe_email,
+    password: Faker::Internet.password(8)
+    )
+  rescue
+    User.create!(
+    username: Faker::Internet.user_name,
+    email: Faker::Internet.safe_email,
+    password: Faker::Internet.password(8)
+    )
+  end
 end
 
 # Following relationships
