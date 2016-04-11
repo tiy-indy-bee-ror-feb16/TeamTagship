bb1ace31f4dbaa313054b28fb075af21aec4da2e
firestorm451 <mzwick1684@hotmail.com>
2016-04-11 04:03:00 -0400

    Was working on blocking buttons, couldn't get it working yet

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160410174856) do
+ActiveRecord::Schema.define(version: 20160411074450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blocks", force: :cascade do |t|
+    t.integer  "blocker"
+    t.integer  "blocked"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "follows", force: :cascade do |t|
     t.string   "follower_type"
