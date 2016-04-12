9f6544c2b579c711a936cbca0aa328d3e8d3bbdc
Ryder Timberlake <wrtimberlake@gmail.com>
2016-04-12 13:48:18 -0400

    Added front-end submit validation, ships with more than 170 chars can't submit

--- app/assets/javascripts/ships.js
+++ app/assets/javascripts/ships.js
@@ -4,11 +4,16 @@ jQuery(document).ready(function($) {
     var remaining = 170 - $('#ship_body').val().length;
     $('.countdown').text(remaining + ' characters remaining.');
     remaining <= 0 ? $('.countdown').css({ 'color': 'red', 'font-size': '150%' }) : $('.countdown').css({ 'color': 'black', 'font-size': '100%' })
-    // if (remaining <= 0) {
-    //   $('.countdown').css({'color': 'red'})
-    // }
-
+    return remaining
   }
+
+  $("#new_ship").submit(function(event) {
+    if (remainingChars() <= 0) {
+      event.preventDefault();
+      alert("That ship is too full to disembark!");
+    }
+  });
+
   remainingChars();
   $('#ship_body').change(remainingChars);
   $('#ship_body').keyup(remainingChars);
