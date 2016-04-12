c85a39afb9c7ea58fd5188d590a432ab01675fe5
Ryder Timberlake <wrtimberlake@gmail.com>
2016-04-12 13:29:50 -0400

    Added js length-checking for ships, including red text for over limit

--- app/assets/javascripts/ships.js
+++ app/assets/javascripts/ships.js
@@ -1,3 +1,15 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+jQuery(document).ready(function($) {
+
+  function remainingChars() {
+    var remaining = 170 - $('#ship_body').val().length;
+    $('.countdown').text(remaining + ' characters remaining.');
+    remaining <= 0 ? $('.countdown').css({ 'color': 'red', 'font-size': '150%' }) : $('.countdown').css({ 'color': 'black', 'font-size': '100%' })
+    // if (remaining <= 0) {
+    //   $('.countdown').css({'color': 'red'})
+    // }
+
+  }
+  remainingChars();
+  $('#ship_body').change(remainingChars);
+  $('#ship_body').keyup(remainingChars);
+});
